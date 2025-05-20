5e3e15f2b5a98978eb226dac9a3e1220b850ca0c
Rian Xavier <rianalvesxvr@gmail.com>
2025-05-20 17:53:25 -0300

    chore: add type support for route components with react-router

--- src/app/pages/Members/MemberDetail.tsx
+++ src/app/pages/Members/MemberDetail.tsx
@@ -1,5 +1,11 @@
-export default function MemberDetail() {
+import type { Route } from './+types/MemberDetail';
+
+export default function MemberDetail(props: Route.ComponentProps) {
+  const { memberId } = props.params;
+
   return (
-    <h1 className="text-3xl font-medium tracking-tight">Membro Detalhe</h1>
+    <h1 className="text-3xl font-medium tracking-tight">
+      Membro Detalhe {memberId}
+    </h1>
   );
 }
